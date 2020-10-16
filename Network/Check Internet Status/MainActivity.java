17a9edaafd9211c10ba1f96dcead2d1c0e47702d
Tulya <33752668+tulya54@users.noreply.github.com>
2020-10-16 14:40:56 +0600

    Update MainActivity.java

--- Network/Check Internet Status/MainActivity.java
+++ Network/Check Internet Status/MainActivity.java
@@ -26,32 +26,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private boolean isInternetStatus(Context context) {
-        boolean result = false;
-        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+  boolean result = false;
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (cm != null) {
-                NetworkCapabilities capabilities = cm.getNetworkCapabilities(connectivityManager.getActiveNetwork());
-                if (capabilities != null) {
-                    if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+            if (connectivityManager != null) {
+                NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.getActiveNetwork());
+                if (networkCapabilities != null) {
+                    if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
                         result = true;
-                    } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                    } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                        result = true;
+                    } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
                         result = true;
                     }
                 }
             }
         } else {
-            if (cm != null) {
-                NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-                if (activeNetwork != null) {
-                    // connected to the internet
+            if (connectivityManager != null) {
+                NetworkInfo activeNetwork = connectivityManager.getActiveNetworkInfo();
+                if (activeNetwork != null && activeNetwork.isConnected()) {
+                    result = true;
+                }
+                /*
                     if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI) {
                         result = true;
                     } else if (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE) {
                         result = true;
                     }
-                }
+                 */
             }
         }
         return result;
     }
 }
+
+
