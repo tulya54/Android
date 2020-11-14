cde29c886612f9a42a11947a2b1128cfbbe0c7fd
Tulya <33752668+tulya54@users.noreply.github.com>
2020-11-14 15:51:43 +0600

    Update MainActivity.java

--- WebView/WebChromeClient/CustomView/MainActivity.java
+++ WebView/WebChromeClient/CustomView/MainActivity.java
@@ -1,1 +1,76 @@
+package com.example.myapplication;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /**/
+        initViews();
+        /**/
+        initWebChromeClient();
+    }
+
+    private void initViews() {
+        webView = findViewById(R.id.webView);
+    }
+
+    private void initWebChromeClient() {
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            private View mCustomView = null;
+            private CustomViewCallback mCustomViewCallback = null;
+            protected FrameLayout mFullscreenContainer = null;
+            private int mOriginalOrientation = 0;
+            private int mOriginalSystemUiVisibility = 0;
+
+            @Nullable
+            @Override
+            public Bitmap getDefaultVideoPoster() {
+                if (mCustomView == null) {
+                    return null;
+                } else {
+                    return BitmapFactory.decodeResource(getApplicationContext().getResources(), 2130837573);
+                }
+            }
+
+            @Override
+            public void onHideCustomView() {
+                ((FrameLayout)getWindow().getDecorView()).removeView(this.mCustomView);
+                this.mCustomView = null;
+                getWindow().getDecorView().setSystemUiVisibility(this.mOriginalSystemUiVisibility);
+                setRequestedOrientation(this.mOriginalOrientation);
+                this.mCustomViewCallback.onCustomViewHidden();
+                this.mCustomViewCallback = null;
+            }
+
+            @Override
+            public void onShowCustomView(View paramView, CustomViewCallback paramCustomViewCallback) {
+                if (this.mCustomView != null) {
+                    onHideCustomView();
+                    return;
+                }
+                this.mCustomView = paramView;
+                this.mOriginalSystemUiVisibility = getWindow().getDecorView().getSystemUiVisibility();
+                this.mOriginalOrientation = getRequestedOrientation();
+                this.mCustomViewCallback = paramCustomViewCallback;
+                ((FrameLayout)getWindow().getDecorView()).addView(this.mCustomView, new FrameLayout.LayoutParams(-1, -1));
+                getWindow().getDecorView().setSystemUiVisibility(3846 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            }
+            
+        });
+    }
+}
