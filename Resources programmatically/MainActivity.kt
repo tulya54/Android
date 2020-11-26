e5bd6e2a128477ed533831f1000a6caf9a381f76
Tulya <33752668+tulya54@users.noreply.github.com>
2020-11-26 13:56:25 +0600

    Update MainActivity.kt

--- Resources programmatically/MainActivity.kt
+++ Resources programmatically/MainActivity.kt
@@ -1,1 +1,27 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(View(this))
+        /**/
+        init()
+    }
+
+    private fun init() {
+        //  A) drawables with theme attributes
+        ContextCompat.getDrawable(this, android.R.drawable.btn_star)
+        //  B) drawables without theme attributes
+        ResourcesCompat.getDrawable(resources, android.R.drawable.btn_star, null)
+        //  C) drawables with theme attributes from another theme
+        ResourcesCompat.getDrawable(resources, android.R.drawable.btn_star, theme)
+    }
+}
 
