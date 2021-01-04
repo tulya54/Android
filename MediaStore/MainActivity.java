a4005f59ab3875916c154037a9b62022d0140da7
Tulya <33752668+tulya54@users.noreply.github.com>
2021-01-04 23:27:26 +0600

    Update MainActivity.java

--- MediaStore/MainActivity.java
+++ MediaStore/MainActivity.java
@@ -27,3 +27,58 @@ public class GetMediaStoreVideos {
         return listOfAllImages;
     }
 }
+
+public class GetMediaStoreImages {
+    public static ArrayList<GalleryPOJO> getAllShownImagesPath(Context context) {
+        Uri uri;
+        Cursor cursor;
+        int column_index_data, column_index_folder_name;
+        ArrayList<GalleryPOJO> listOfAllImages = new ArrayList<>();
+        String absolutePathOfImage = null, title;
+        uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
+        String[] projection = { MediaStore.MediaColumns.DATA, MediaStore.Images.Media.DISPLAY_NAME };
+
+        cursor = context.getContentResolver().query(uri, projection, null,
+                null, null);
+
+        column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+     //   column_index_folder_name = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
+        column_index_folder_name = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DISPLAY_NAME);
+        while (cursor.moveToNext()) {
+            absolutePathOfImage = cursor.getString(column_index_data);
+            title = cursor.getString(column_index_folder_name);
+            listOfAllImages.add(new GalleryPOJO(title, absolutePathOfImage, 1, false));
+        }
+        return listOfAllImages;
+    }
+}
+
+public class GetMediaStoreAudio {
+    public static ArrayList<GalleryPOJO> getAllShownImagesPath(Context context) {
+
+        ArrayList<GalleryPOJO> listOfAllImages = new ArrayList<>();
+        ContentResolver contentResolver = context.getContentResolver();
+        Uri uri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor cur = contentResolver.query(uri, null, null, null, null);
+
+        if (cur.moveToFirst()) {
+            do {
+                int pathIndex = cur.getColumnIndex(MediaStore.Audio.Media.DATA);
+
+                int nameIndex = cur.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME);
+
+                String spath = cur.getString(pathIndex);
+                String name = cur.getString(nameIndex);
+                //    paths.add(spath.substring(4));
+                //   songs.add(name);
+                listOfAllImages.add(new GalleryPOJO(name, spath, 3, false));
+            } while (cur.moveToNext());
+
+
+
+
+        }
+        return listOfAllImages;
+    }
+}
