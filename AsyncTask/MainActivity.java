248cb0b28c2b9b11df2c5d7bab4dd5438b583524
Tulya <33752668+tulya54@users.noreply.github.com>
2021-01-04 23:49:33 +0600

    Create MainActivity.java

---
+++ AsyncTask/MainActivity.java
@@ -0,0 +1,6 @@
+AsyncTask.execute(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        daoNew.insert(new NewStories(sNew1, sNew2));
+                                    }
+                                });*/
