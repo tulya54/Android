99b920fb1fff577150532e4d1fff759fa5eb84a4
Tulya <33752668+tulya54@users.noreply.github.com>
2021-01-04 22:13:37 +0600

    Create MainActivity.java

---
+++ Device information/MainActivity.java
@@ -0,0 +1,29 @@
+Example 
+
+String result = Build.MODEL;
+
+Build.BOARD = MSM8974
+Build.BOOTLOADER = s1
+Build.BRAND = Sony
+Build.CPU_ABI = armeabi-v7a
+Build.CPU_ABI2 = armeabi
+Build.DEVICE = D5503
+Build.DISPLAY = 14.6.A.1.236
+Build.FINGERPRINT = Sony/D5503/D5503:5.1.1/14.6.A.1.236/2031203XXX:user/release-keys
+Build.HARDWARE = qcom
+Build.HOST = BuildHost
+Build.ID = 14.6.A.1.236
+Build.IS_DEBUGGABLE = false
+Build.MANUFACTURER = Sony
+Build.MODEL = D5503
+Build.PRODUCT = D5503
+Build.RADIO = unknown
+Build.SERIAL = CB5A1YGVMT
+Build.SUPPORTED_32_BIT_ABIS = [Ljava.lang.String;@3dd90541
+Build.SUPPORTED_64_BIT_ABIS = [Ljava.lang.String;@1da4fc3
+Build.SUPPORTED_ABIS = [Ljava.lang.String;@525f635
+Build.TAGS = release-keys
+Build.TIME = 144792559XXXX
+Build.TYPE = user
+Build.UNKNOWN = unknown
+Build.USER = BuildUser
