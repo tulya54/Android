f59c18853725ded3fa297a1f86e33e6e307343a5
Tulya <33752668+tulya54@users.noreply.github.com>
2021-02-06 01:26:58 +0600

    Update MainActivity.kt

--- Base Classes/MainActivity.kt
+++ Base Classes/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.myapplication
 
 class MainActivity : BaseActivity(R.layout.activity_main) {
 
+    companion object{
+        const val TAG = "MainActivity"
+    }
+    
     override fun initViews() {
         /*
          *  UI created
