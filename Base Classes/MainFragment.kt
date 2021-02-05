54d4461979066b4ee39f6f24a9ad71f48e1c7707
Tulya <33752668+tulya54@users.noreply.github.com>
2021-02-06 01:56:22 +0600

    Create MainFragment.kt

---
+++ Base Classes/MainFragment.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import android.view.View
+
+class MainFragment: BaseFragment(R.layout.fragment_main) {
+
+    companion object{
+        const val TAG = "MainFragment"
+    }
+
+    override fun initViews(view: View) {
+        /*
+         *  UI created
+         *  your code
+         */
+    }
+}
