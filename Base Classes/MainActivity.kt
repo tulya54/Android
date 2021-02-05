44b5f677c7efbb12ae33005d13e22c0a0cace756
Tulya <33752668+tulya54@users.noreply.github.com>
2021-02-06 01:25:11 +0600

    Create MainActivity.kt

---
+++ Base Classes/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+class MainActivity : BaseActivity(R.layout.activity_main) {
+
+    override fun initViews() {
+        /*
+         *  UI created
+         *  your code
+         */
+    }
+}
