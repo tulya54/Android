a1f0faacf816f0fd650029ddb561de86b3fbe1e8
Tulya <33752668+tulya54@users.noreply.github.com>
2021-02-06 02:07:24 +0600

    Create MainFragment.java

---
+++ Base Classes/MainFragment.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import android.view.View;
+
+public class MainFragment extends BaseFragment {
+
+    private static final String TAG = "MainFragment";
+
+    @Override
+    protected int getLayoutResID() {
+        return R.layout.fragment_main;
+    }
+
+    @Override
+    protected void initViews(View view) {
+
+    }
+}
