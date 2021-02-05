7763f68246ade05752459032371b552b150ad030
Tulya <33752668+tulya54@users.noreply.github.com>
2021-02-06 02:04:49 +0600

    Create MainActivity.java

---
+++ Base Classes/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+public class MainActivity extends BaseActivity {
+
+    public static final String TAG = "MainActivity";
+
+    @Override
+    protected int getLayoutResID() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initViews() {
+        /*
+         *  UI created
+         *  your code
+         */
+    }
+}
