ecf652a71f5d49b5c2eab603ecae044f4b233564
Tulya <33752668+tulya54@users.noreply.github.com>
2021-02-07 01:39:38 +0600

    Update MainFragment.kt

--- Base Binding Classes/MainFragment.kt
+++ Base Binding Classes/MainFragment.kt
@@ -1,1 +1,20 @@
+package com.example.myapplication
 
+import android.view.View
+import com.example.myapplication.databinding.FragmentMainBinding
+
+class MainFragment: BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
+
+    companion object{
+        const val TAG = "MainFragment"
+        const val TEXT = "Hello World!"
+    }
+
+    override fun initViews(view: View) {
+        /*
+         *  UI created
+         *  your code
+         */
+        binding?.tvText?.text = TEXT
+    }
+}
