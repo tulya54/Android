a8fac4c2419c4d4d9fccefae376324bd3a0bb804
Tulya <33752668+tulya54@users.noreply.github.com>
2021-02-06 15:57:53 +0600

    Create MainFragment.java

---
+++ Base Binding Classes/MainFragment.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.view.View;
+import com.example.myapplication.databinding.FragmentMainBinding;
+
+public class MainFragment extends BaseBindingFragment<FragmentMainBinding> {
+
+    private static final String TAG = "MainFragment";
+    private static final String TEXT = "Hello World!";
+
+    @Override
+    protected int getLayoutResID() {
+        return R.layout.fragment_main;
+    }
+
+    @Override
+    protected void initViews(View view) {
+        binding.tvText.setText(TEXT);
+    }
+}
