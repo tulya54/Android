1af20281ae8533350fe6fd15ac8bedc9f57322c2
Tulya <33752668+tulya54@users.noreply.github.com>
2021-02-06 14:58:03 +0600

    Update MainActivity.java

--- Base Binding Classes/MainActivity.java
+++ Base Binding Classes/MainActivity.java
@@ -1,1 +1,19 @@
+package com.example.myapplication;
 
+import com.example.myapplication.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseBindingActivity<ActivityMainBinding> {
+
+    private static final String TAG = "MainActivity";
+    private static final String TEXT = "Hello World!";
+
+    @Override
+    protected int getLayoutResID() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initViews() {
+        binding.tvText.setText(TEXT);
+    }
+}
