5f3646149b6828881b16833aaafa811b0d03e58b
Tulya <33752668+tulya54@users.noreply.github.com>
2021-02-09 15:22:27 +0600

    Update MainActivity.java

--- Keyboard/MainActivity.java
+++ Keyboard/MainActivity.java
@@ -1,1 +1,15 @@
-
+ @SuppressLint("ClickableViewAccessibility")
+    private void onTouchPassword() {
+        etPassword.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                scrollView.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+                    }
+                }, 300);
+                return false;
+            }
+        });
+    }
