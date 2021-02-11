7adab56e34018bde89feca850ef2e6ed5ef2e12a
Tulya <33752668+tulya54@users.noreply.github.com>
2021-02-12 04:26:30 +0600

    Update BaseFragment.java

--- Base Classes/BaseFragment.java
+++ Base Classes/BaseFragment.java
@@ -17,7 +17,7 @@ import androidx.fragment.app.Fragment;
 
 public abstract class BaseFragment extends Fragment {
 
-    public static final String TAG = "BaseActivity";
+    private static final String TAG = "BaseActivity";
     //  Your layout
     @LayoutRes
     protected abstract int getLayoutResID();
