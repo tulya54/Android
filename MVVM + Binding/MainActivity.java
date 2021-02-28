bcdff550393d5ab4b404fa5eda3661acb504ab62
Tulya <33752668+tulya54@users.noreply.github.com>
2021-02-28 17:16:24 +0600

    Update MainActivity.java

--- MVVM + Binding/MainActivity.java
+++ MVVM + Binding/MainActivity.java
@@ -1,1 +1,36 @@
+package com.example.myapplication;
 
+import com.example.myapplication.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity<MainViewModel, ActivityMainBinding> {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected Class<MainViewModel> getViewModel() {
+        return MainViewModel.class;
+    }
+
+    @Override
+    protected int getLayoutResID() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initViews() {
+        viewModel.getResponse().observe(this, this::consumeResponse);
+        viewModel.getClickResponse().observe(this, this::onClickResponse);
+    }
+
+    //  Api response
+    private void consumeResponse(ApiResponse apiResponse) {
+
+    }
+
+    //  Click action
+    private void onClickResponse(int btn) {
+        switch (btn) {
+
+        }
+    }
+}
