67964137b74ffb6feba03adc8f8802d74783c6fe
Tulya <33752668+tulya54@users.noreply.github.com>
2021-03-28 16:52:16 +0600

    Update FirstFragment.java

--- FragmentManager/FirstFragment.java
+++ FragmentManager/FirstFragment.java
@@ -1,6 +1,12 @@
 package com.example.myapplication;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class FirstFragment extends Fragment {
