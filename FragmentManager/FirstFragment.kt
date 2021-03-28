c4f438e44e7dbb53156b69ddc12846d9f708851b
Tulya <33752668+tulya54@users.noreply.github.com>
2021-03-28 16:57:44 +0600

    Update FirstFragment.kt

--- FragmentManager/FirstFragment.kt
+++ FragmentManager/FirstFragment.kt
@@ -1,1 +1,44 @@
+package com.example.myapplication
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+class FirstFragment: Fragment() {
+
+    companion object {
+        fun newInstance(text: String, index: Int): FirstFragment {
+            val args = Bundle()
+            args.putString("text", text)
+            args.putInt("index", index)
+            val fragment = FirstFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    fun onBegin() {
+        Toast.makeText(activity, "onBegin", Toast.LENGTH_SHORT).show()
+    }
+
+    fun onBack() {
+        if (activity!!.supportFragmentManager.backStackEntryCount > 0) {
+            activity!!.supportFragmentManager.popBackStack()
+            val fragment: Fragment? = activity!!.supportFragmentManager.findFragmentById(R.id.frame)
+            fragment?.let {
+                if (fragment is FirstFragment) {
+                    activity!!.supportFragmentManager.beginTransaction().remove(fragment)
+                }
+            }
+        } else {
+            activity!!.supportFragmentManager.beginTransaction().remove(this)
+        }
+    }
+}
